eddbc30014ccdf573fbae51d23476bdb3ad84de0
Jared Odulio <jared@Jareds-MacBook-Pro.local>
2021-05-22 08:59:28 +0800

    added sidebar code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret , faAngleDoubleRight, faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret, faAngleDoubleRight, faCloudUploadAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+  defaultContainerElement: '#content'
+});
 
 Vue.config.productionTip = false
 
