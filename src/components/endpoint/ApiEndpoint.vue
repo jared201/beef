3d8ec5c742a81185bfa9f5ed94268b0bf6a71173
Jared Odulio <jared.odulio@gmail.com>
2023-04-27 17:44:29 +0800

    added endpoint component

--- src/components/endpoint/ApiEndpoint.vue
+++ src/components/endpoint/ApiEndpoint.vue
@@ -2,13 +2,11 @@
 <div class="container">
     <div class="columns">
         <div class="column">
-            First column
             <div class="columns">
                 <p>Left section</p>
             </div>
         </div>
         <div class="column">
-            Second column
             <div class="columns">
                 <p>First section</p>
             </div>
