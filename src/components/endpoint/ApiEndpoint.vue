a21332be729f1e3b98df53264146cc5b280aadcf
Jared Odulio <jared.odulio@gmail.com>
2023-04-27 19:23:06 +0800

    added sections to the beef API page

--- src/components/endpoint/ApiEndpoint.vue
+++ src/components/endpoint/ApiEndpoint.vue
@@ -1,17 +1,20 @@
 <template>
 <div class="container">
     <div class="columns">
-        <div class="column">
-            <div class="columns">
-                <p>Left section</p>
+        <div class="column section has-background-black">
+            <p class="is-size-2 has-text-white">Beef Server API</p>
+            <div class="panel is-justify-content-left">
+            <p class="has-text-white is-pulled-left">
+                {{description}}
+            </p>
             </div>
         </div>
         <div class="column">
-            <div class="columns">
-                <p>First section</p>
+            <div class="columns section is-primary">
+                <p class="is-size-2">First section</p>
             </div>
-            <div class="columns">
-                <p>Second section</p>
+            <div class="columns section has-background-primary">
+                <p class="is-size-2">Second section</p>
             </div>
         </div>
     </div>
@@ -20,7 +23,14 @@
 
 <script>
 export default {
-    name: "ApiEndpoint"
+    name: "ApiEndpoint",
+    data() {
+        return {
+            clicked: false,
+            is_active: "",
+            description: "This is the Beef API endpoint page. It will contain all the information about the API endpoints and how to use them.",
+        }
+    },
 }
 </script>
 
